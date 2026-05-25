409c1c001f487733d7eeb48c80ada1a25070d774
Rubén Rodríguez Navarro <ruben_rodri16@hotmail.com>
2026-05-25 08:07:58 +0200

    Create camera.py

---
+++ src/camera.py
@@ -0,0 +1,17 @@
+import cv2
+
+class Camera:
+
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    def get_frame(self):
+        ret, frame = self.cap.read()
+
+        if not ret:
+            return None
+
+        return frame
